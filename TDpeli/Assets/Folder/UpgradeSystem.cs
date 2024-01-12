ccfe01355d2cfb1ad76c5ff8d761b5b06f0ae5f2
kukkakakka <111349064+kukkakakka@users.noreply.github.com>
2024-01-12 10:29:20 +0200

    torni ampuu ja sitä rataa

--- TDpeli/Assets/Folder/UpgradeSystem.cs
+++ TDpeli/Assets/Folder/UpgradeSystem.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeSystem : MonoBehaviour
 {
+    public KatsoJaAmpu katsoJaAmpu;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class UpgradeSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp("space"))
+        {
+            upgradetorni1();
+        }
+    }
+
+    void upgradetorni1()
+    {
+        katsoJaAmpu.ampumisVali = katsoJaAmpu.ampumisVali / 2;
     }
 }
